4d426be44d382f6e3fae38fe84aaf0597ba815be
gwuah <griffithawuah15@gmail.com>
2025-08-06 05:01:26 +0200

    setup server & add graceful shutdown

---
+++ src/handler.rs
@@ -0,0 +1,15 @@
+use axum::{Json, response::IntoResponse};
+use serde::{Deserialize, Serialize};
+use tracing::info;
+
+#[derive(Debug, Serialize, Default)]
+struct Response {
+    status: String,
+}
+
+pub async fn healthcheck() -> impl IntoResponse {
+    info!("got healthcheck request");
+    Json(Response {
+        status: "ok".to_string(),
+    })
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    Router,
+    // extract::{Path, Query, State},
+    // http::StatusCode,
+    // response::IntoResponse,
+    routing::get,
+};
+use bibliotek::handler::healthcheck;
+// use std::future::Future;
+use tokio::{signal, sync::broadcast};
+use tracing::{error, info};
+// use serde::{Deserialize, Serialize};
+
+struct Config {
+    port: String,
+}
+
+impl Config {
+    fn new() -> Self {
+        // to load from environment variables
+        // hardcoded for now.
+        Config {
+            port: "5678".to_string(),
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt().json().init();
+    info!("bibliotek.svc starting");
+
+    let cfg = Config::new();
+    let address = format!("0.0.0.0:{}", cfg.port.clone());
+
+    let (sender, _) = broadcast::channel::<()>(1);
+    let app = Router::new().route("/", get(healthcheck));
+    let listener = tokio::net::TcpListener::bind(&address)
+        .await
+        .unwrap_or_else(|e| {
+            error!(error = %e, "failed to setup tcp listener");
+            std::process::exit(1);
+        });
+
+    info!("bibliotek.svc running on {}", &address);
+    tokio::select! {
+        result = axum::serve(listener, app) => {
+            if let Err(err) = result {
+                error!(error = %err, "failed to setup tcp listener");
+                std::process::exit(1);
+            }
+        }
+        _ = signal::ctrl_c() => {
+            info!("ctrl+c signal received, preparing to shutdown");
+            _ = sender.send(());
+        }
+    }
 }
